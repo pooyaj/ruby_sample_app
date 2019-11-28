f85fcdc4ce54420f74000652392b67e1dbc3ada1
Pooya Jaferian <pooya.j@gmail.com>
2019-11-28 11:28:12 -0800

    added signup fllow

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users
 end
